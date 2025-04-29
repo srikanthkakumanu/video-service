3a6d3908d6e8725dc842df0ece722697b1a54e89
Srikanth Kakumanu <srikanthkakumanu@yahoo.com>
2025-04-29 12:53:58 +0530

    Remove Eureka client annotations and update application configurations

--- src/main/java/videos/VideosApplication.java
+++ src/main/java/videos/VideosApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class VideosApplication {
     public static void main(String... args) {
         SpringApplication.run(VideosApplication.class, args);
